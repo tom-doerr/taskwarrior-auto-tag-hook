2c8c59ad8185ce7cfdd8d2916bc7f50ef91200fc
Tom Dörr <tom.doerr@tum.de>
2021-06-27 00:47:01 +0200

    Add support for recurring tag adding

--- auto_tag.py
+++ auto_tag.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import logging
+if False:
+    logging.basicConfig(filename='auto_tag.log', filemode='w', level=logging.ERROR)
+
 import json
 import sys
 from pathlib import Path
+
 
 OLD = json.loads(sys.stdin.readline())
 
@@ -12,7 +17,6 @@ except Exception:
     pass
 
 
-#print('Input: ' + str(OLD))
 new = OLD.copy()
 
 with open(str(Path.home()) + '/.task/hooks/tags.config', 'r') as tag_config_file:
@@ -23,18 +27,22 @@ if 'tags' not in list(OLD):
     print(json.dumps(new))
     exit()
 
+num_tags_old = 0
+while True:
+    logging.debug(f"num_tags_old: {num_tags_old}")
+    if num_tags_old == len(new['tags']):
+        break
+    num_tags_old = len(new['tags'])
+    for tag1 in list(TAG_CONFIG):
+        if tag1 in list(new['tags']):
+            if type(TAG_CONFIG[tag1]) is list:
+                for tag_to_add in TAG_CONFIG[tag1]:
+                    if tag_to_add not in new['tags']:
+                        new['tags'].append(tag_to_add)
+            else:
+                if TAG_CONFIG[tag1] not in new['tags']:
+                    new['tags'].append(TAG_CONFIG[tag1])
 
-for tag1 in list(TAG_CONFIG):
-    if tag1 in list(OLD['tags']):
-        #print('Found ' + str(tag1) + ' in tags')
-        if type(TAG_CONFIG[tag1]) is list:
-            for tag_to_add in TAG_CONFIG[tag1]:
-                new['tags'].append(tag_to_add)
-        else:
-            new['tags'].append(TAG_CONFIG[tag1])
 
-
-
-#print('Output: ' + str(new))
 
 print(json.dumps(new))
